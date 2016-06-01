7ef55b275b442cc09c8e8a3a6684508594fb6fd2
Ivan5646 <Mike000027@yandex.ru>
2016-06-01 15:00:22 +0600

    specs update

--- spec/api/v1/articles_controller_spec.rb
+++ spec/api/v1/articles_controller_spec.rb
@@ -16,13 +16,6 @@ RSpec.describe Api::V1::ArticlesController, type: :controller do
   describe 'POST create' do
     subject{post :create, article: {title: 'name', text: 'text'}}
 
-    context "if status is ok" do
-      before(:each){post :create, article: {title: 'name', text: 'text'}}
-      it "has a 201 status code" do
-      expect(response.status).to eq 201
-      end
-    end
-
     context "check if the article was created in DB" do
       it {expect{subject}.to change{Article.count}.by(1)}
         it 'status is ok' do
@@ -31,22 +24,27 @@ RSpec.describe Api::V1::ArticlesController, type: :controller do
         end
     end
 
-    # ?
-    context 'invalid attributes' do
-      let(:attributes){{ text: 'title'}}
-      it {expect{subject}.not_to change{Article.count}}
-      it 'status is 422' do
+    context 'valid attributes' do
+      let(:attributes){{title: 'name', text: 'text'}}
+      it {expect{subject}.to change{Article.count}.by(1)}
+      it 'status is ok' do
         subject
-        expect(response.status).to eq 422
+        expect(response.status).to eq 201
       end
     end
 
+    # ?
+    #context 'invalid attributes' do
+      #let(:attributes){{ text: 'title'}}
+      #it {expect{subject}.not_to change{Article.count}}
+      #it 'status is 422' do
+      #  subject
+      #  expect(response.status).to eq 422
+      #end
+   # end
 
   end
 end
-
-
-
 
 
 
